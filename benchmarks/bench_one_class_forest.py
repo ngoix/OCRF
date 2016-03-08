402f008eaf103ecb9e1006cccd790e4c7d80c7b4
Nicolas Goix <goix.nicolas@gmail.com>
2016-03-08 15:23:03 +0100

    up benchmark

--- benchmarks/bench_one_class_forest.py
+++ benchmarks/bench_one_class_forest.py
@@ -16,15 +16,16 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest, OneClassForest
 from sklearn.metrics import roc_curve, auc
 from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_mldata
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelBinarizer, scale
 from sklearn.utils import shuffle as sh
+from sklearn import grid_search
 
 
-np.random.seed(1)
+np.random.seed(0)
 
-
-datasets = ['http', 'smtp', 'SA', # 'SF',
-            'shuttle', 'forestcover']
+# ['http', 'smtp', 'SA', 'SF', 'shuttle', 'forestcover']
+# continuous datasets: http, smtp, shuttle, forescover
+datasets = ['shuttle'] #['http', 'smtp', 'shuttle', 'forestcover'] 
 
 for dat in datasets:
     # loading and vectorization
@@ -81,18 +82,49 @@ for dat in datasets:
         y = (y != 'normal.').astype(int)
 
     n_samples, n_features = np.shape(X)
-    n_samples_train = n_samples // 2
+    n_samples_train =  n_samples // 2
     n_samples_test = n_samples - n_samples_train
 
     X = X.astype(float)
+    X = scale(X)
+    # remove useless features:
+    useless_features = (X.max(axis=0) - X.min(axis=0))==0
+    for j in reversed(range(n_features)):
+        if useless_features[j]:
+            X = np.delete(X, (j), axis=1)
+
     X_train = X[:n_samples_train, :]
     X_test = X[n_samples_train:, :]
     y_train = y[:n_samples_train]
     y_test = y[n_samples_train:]
 
+    
+    ### cross-val: ####
+    #parameters = {'max_samples':[.05, .1, .2, .3], 'max_features':[2, 5, 8, 10, 15], 'n_estimators':[5, 10, 20]}   #too much parameters yields segmentation error
+    parameters = {'max_depth':['auto', 10, 100, 1000
+                           ], 'max_samples':[.05, .1, 'auto'], 'max_features':[min(10, n_features)], 'n_estimators':[20, 50]}
+    # good param: .05,10,20  (-> auc:0.977)  aussi: 0.02,10,70 aussi: max_depth=1000 et 0.5,8,70
+    model = OneClassForest()
+    clf = grid_search.GridSearchCV(model, parameters, refit=False,cv=2)
+    clf.fit(X_train, y_train)
+    print 'clf.best_params_', clf.best_params_
+    model.set_params(**clf.best_params_)
+
+    ################### bench results:
+    #http: {'max_features': 3, 'max_samples': 'auto', 'n_estimators': 20, 'max_depth': 100} -> AUC 0.997 (alpha=1), 0.998 (alpha=0.1) (opt parameters unchanged)
+    #smtp: {'max_features': 3, 'max_samples': 0.1, 'n_estimators': 50, 'max_depth': 100} -> AUC 0.98 (alpha=1), 0.98 (alpha=0.1)
+    #forestcover: {'max_features': 10, 'max_samples': 0.05, 'n_estimators': 50, 'max_depth': 1000} ->AUC 0.966 (alpha=1.), 0.967 (alpha=0.1)
+    #shuttle: (segm error when too large max_depth): {'max_features': 9, 'max_samples': 0.1, 'n_estimators': 20, 'max_depth': 10} --> AUC 0.97
+    ###################
+
     print('OneClassForest processing...')
-    model = OneClassForest()  # n_jobs=-1)
+    #model = OneClassForest(max_depth='auto', max_samples=0.05, max_features=min(8,n_features), n_estimators=50)  # n_jobs=-1)  #commented since cross val
     tstart = time()
+
+    # ### training only on normal data:
+    # X_train = X_train[y_train==0]
+    # y_train = y_train[y_train==0]
+
     model.fit(X_train)
     fit_time = time() - tstart
     tstart = time()
